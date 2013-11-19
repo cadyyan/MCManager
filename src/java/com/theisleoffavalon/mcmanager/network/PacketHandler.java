e42e6c62c88f8201ad6a380d98a89da319125179
Cadyyan <cadyyan@Avalon>
2013-11-19 12:16:14 -0600

    Imports are dumb and so am I

--- src/java/com/theisleoffavalon/mcmanager/network/PacketHandler.java
+++ src/java/com/theisleoffavalon/mcmanager/network/PacketHandler.java
@@ -1,6 +1,11 @@
 package com.theisleoffavalon.mcmanager.network;
 
 import cpw.mods.fml.common.network.IPacketHandler;
+import cpw.mods.fml.common.network.Player;
+
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.Packet250CustomPayload;
 
 public class PacketHandler implements IPacketHandler
 {
